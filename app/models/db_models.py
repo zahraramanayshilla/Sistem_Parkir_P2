f2717d42614d1dfaeab127e10fa6a1e0dd6080e7
zara <zaraasabilaa@gmail.com>
2025-12-08 16:53:46 +0700

    hm

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -1,4 +1,3 @@
-# app/models/db_models.py
 from mongoengine import (
     Document,
     StringField,
@@ -6,29 +5,21 @@ from mongoengine import (
     DateTimeField,
     ReferenceField,
 )
+from mongoengine import Document, StringField, EmailField, DateTimeField, ReferenceField
 from datetime import datetime
 
+from mongoengine import Document, StringField, DateTimeField, ReferenceField
+from datetime import datetime
 
 class User(Document):
-    meta = {
-        "collection": "users",  # nama collection di MongoDB
-    }
-
-    username = StringField(required=True, unique=True, max_length=100)
-    email = EmailField(required=True, unique=True, max_length=120)
-    password = StringField(required=True, max_length=200)
-    role = StringField(default="mahasiswa", max_length=20)
-    created_at = DateTimeField(default=datetime.utcnow)
+    username = StringField(required=True, unique=True)
+    email = StringField(required=True, unique=True)
+    password = StringField(required=True)
+    role = StringField(default="petugas")  # atau "admin"
 
 
 class ParkirLog(Document):
-    meta = {
-        "collection": "parkir_logs",
-    }
-
-    # relasi ke User (ganti user_id integer → ReferenceField)
     user = ReferenceField(User, required=True)
-
     waktu_masuk = DateTimeField(default=datetime.utcnow)
-    waktu_keluar = DateTimeField(null=True)
-    status = StringField(default="masuk", max_length=20)
+    waktu_keluar = DateTimeField()       # boleh kosong
+    status = StringField(default="masuk")  # "masuk" / "keluar"
